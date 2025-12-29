f54bf0f1df42531b742f9a5158e5980930d5281e
old2250-arch <old2250@gmail.com>
2025-12-29 21:20:12 +0700

    Create Wilzz.js

---
+++ img/Wilzz.js
@@ -0,0 +1,19 @@
+OWNER_ID: "8261277275", // ID Telegram owner
+urladmin: "https://t.me/wilzzofficial",
+urlch: "https://t.me/wilzzofficial_channel", 
+NOTIF_CHANNEL: -1003267408771, 
+botName: "AutoOrder - Vps",
+version: "1.0.0",
+authorName: "@wilzzofficial",
+// config.js
+  REQUIRED_CHANNELS: [
+   '@WilzzOfficial_Channel', // Ganti dengan username channel pertama
+   '@WilzzOfficial_testimoni', // Ganti dengan username channel kedua  
+   '@wilzzofficial_channel3'
+  ]
+};  
+//==============================================[ SETTING IMAGE ]=======//
+ppthumb: "https://files.catbox.moe/wcwkwo.jpg",       // Foto utama bot (/start)
+ppdigitalocean: "https://files.catbox.moe/wcwkwo.jpg",
+
+//==============================================[ SETTING EMOJI ID ]=======//
