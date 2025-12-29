1a5761fbb3efeb3b8f4c58f5ad52948b0b826c32
old2250-arch <old2250@gmail.com>
2025-12-29 21:32:15 +0700

    Update _keys.js

--- api/_keys.js
+++ api/_keys.js
@@ -6,16 +6,16 @@ export const KEYS = {
   },
 
   pterodactyl: {
-    domain: "https://ntraja",
-    apiKey: "ntargwisi",
-    clientKey: "ptlc_VGN3HVCT6G9FuUoKkkmWdSl5j6pKLCXye6pYpFKHbfY",
+    domain: "https://panel.wilzzoffc.biz.id",
+    apiKey: "ptla_r9jy5toTlKzqaao4xLoaEYwS1AyUICNWljNHR1afNAd",
+    clientKey: "ptlc_Voi0sLGgegUxCXaa1niJ7aBMMXakNGujJp3KyfMyrct",
     egg: 15,
     nestId: 5,
     locationId: 1,
   },
 
   digitalocean: {
-    apiKey: "",
+    apiKey: "dop_v1_935d9f8a0796855f5396778e66f8e09ed79ac08d520245e3d763da287bc17b6e",
     region: "sgp1",
     image: "ubuntu-24-04-x64",
   },
