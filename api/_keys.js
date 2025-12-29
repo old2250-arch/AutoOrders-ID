803822021676c032d158372dff776f3608aab2c7
old2250-arch <old2250@gmail.com>
2025-12-29 21:13:39 +0700

    Create _keys.js

---
+++ api/_keys.js
@@ -0,0 +1,22 @@
+export const KEYS = {
+  pakasir: {
+    project: "WilzzOrder",
+    apiKey: "QSl1iHQodFLg7Bc3gjsROfvJxgWY3UQl",
+    baseUrl: "https://app.pakasir.com/api",
+  },
+
+  pterodactyl: {
+    domain: "https://ntraja",
+    apiKey: "ntargwisi",
+    clientKey: "ptlc_VGN3HVCT6G9FuUoKkkmWdSl5j6pKLCXye6pYpFKHbfY",
+    egg: 15,
+    nestId: 5,
+    locationId: 1,
+  },
+
+  digitalocean: {
+    apiKey: "",
+    region: "sgp1",
+    image: "ubuntu-24-04-x64",
+  },
+};
